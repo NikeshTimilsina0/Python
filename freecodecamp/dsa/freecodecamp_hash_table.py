77bcbfe32caea1f043141fb64d9297e0329d0b9e
Nikesh Timilsina <nikeshtimilsina3@gmail.com>
2026-02-08 10:00:53 +0545

    Added freecodecamp_merge_sort in dsa folder

--- freecodecamp/dsa/freecodecamp_hash_table.py
+++ freecodecamp/dsa/freecodecamp_hash_table.py
@@ -1,47 +1,49 @@
 class HashTable:
-    def __init__(self):
-        self.collection={}
-    def hash(self,string):
-        hash_sum=0
-        for char in string:
-            hash_sum+=ord(char)
-        return hash_sum
-        
-    def add(self,key,value)->None:
+    def __init__(self): # initializes the hash table object
+        self.collection = {} # the main dictionary to store hashed buckets
 
-        key_hash=self.hash(key)
-        if key_hash not in self.collection:
-            self.collection[key_hash]={}
-        self.collection[key_hash][key]=value
+    def hash(self, string): # creates a numerical index based on the characters in a string
+        hash_sum = 0 # starts the sum at zero
+        for char in string: # loops through every character in the word
+            hash_sum += ord(char) # adds the numerical Unicode value of the character to the sum
+        return hash_sum # returns the final sum as the hash key
         
-    
-    def remove(self,key):
-        key_hash=self.hash(key)
-        if key_hash in self.collection:
-            if key in self.collection[key_hash]:
-                del self.collection[key_hash][key]
-        return
+    def add(self, key, value) -> None: # adds a new key-value pair to the table
+        key_hash = self.hash(key) # generates a hash for the provided key
+        if key_hash not in self.collection: # check if this hash 'bucket' already exists
+            self.collection[key_hash] = {} # if not, create a new dictionary at that hash location
+        self.collection[key_hash][key] = value # store the actual key and value inside that hash bucket
         
-    def lookup(self,key):
-        key_hash=self.hash(key)
-        if key_hash not in self.collection:
+    def remove(self, key): # removes an entry from the table using its key
+        key_hash = self.hash(key) # calculate the hash to find where the key would be stored
+        if key_hash in self.collection: # check if the hash bucket exists
+            if key in self.collection[key_hash]: # check if the specific key exists in that bucket
+                del self.collection[key_hash][key] # delete the key-value pair
+        return # exit the function
+        
+    def lookup(self, key): # finds and returns the value associated with a key
+        key_hash = self.hash(key) # calculate the hash to know which bucket to look in
+        if key_hash not in self.collection: # if the hash doesn't exist, the key isn't there
             return None
-        if key in self.collection[key_hash]:
-            return self.collection[key_hash][key]
-        
+        if key in self.collection[key_hash]: # if the key exists in the bucket
+            return self.collection[key_hash][key] # return the stored value
+
+# --- Testing the HashTable ---
 hashing = HashTable()
 
-print(hashing.hash('golf'))
+print(hashing.hash('golf')) # prints the numerical hash of 'golf' (432)
 
-hashing.add('golf', 'sport')
+hashing.add('golf', 'sport') # adds 'golf' under hash 432
 print(hashing.collection)
 
+# 'dear' and 'read' have the same letters, so they will 'collide' at the same hash
 hashing.add('dear', 'friend')
 hashing.add('read', 'book')
-print(hashing.collection)
+print(hashing.collection) # you will see both stored under the same hash key
 
-print(hashing.lookup('golf'))
+print(hashing.lookup('golf')) # searches and returns 'sport'
 
+# 'fcc' and 'cfc' are another example of a collision
 hashing.add('fcc', 'coding')
 hashing.add('cfc', 'chemical')
 print(hashing.collection)
--- freecodecamp/dsa/freecodecamp_linkedlist.py
+++ freecodecamp/dsa/freecodecamp_linkedlist.py
@@ -1,48 +1,53 @@
 class LinkedList:
-    class Node:
-        def __init__(self, element):
+    class Node: # this stores the data in the linked list
+        def __init__(self, element): # initial values when an object of this class Node is instantiated
             self.element = element
-            self.next = None
-            
-    def __init__(self):
-        self.length = 0
-        self.head = None
+            self.next = None # when a node is first created, it points to nothing
 
-    def is_empty(self):
-        return self.length == 0
+    def __init__(self): # when an object of LinkedList class is created, length is set to 0 and first element is set to None
+        self.length = 0 # initial length
+        self.head = None # initial first element
+
+    def is_empty(self): # if the linked list is empty, check and return true or false
+        return self.length == 0 
     
     def add(self, element):
-        node = self.Node(element)
+        node = self.Node(element) # Node object instantiated passing the elements
         if self.is_empty():
-            self.head = node
+            self.head = node # if the list is empty, make this new node the first element
         else:
-            current_node = self.head
-            while current_node.next is not None:
+            current_node = self.head # start at the beginning of the list
+            while current_node.next is not None: # loop through until we reach the last node
                 current_node = current_node.next
-            current_node.next = node
-        self.length += 1
+            current_node.next = node # link the last node's 'next' pointer to our new node
+        self.length += 1 # increase the list size counter
 
     def remove(self, element):
-        previous_node = None
-        current_node = self.head
+        previous_node = None # keeps track of the node right before the one we want to delete
+        current_node = self.head # start searching from the beginning of the list
+        
+        # loop until we find the element or reach the end of the list
         while current_node is not None and current_node.element != element:
-            previous_node = current_node
-            current_node = current_node.next
-        if current_node is None:
+            previous_node = current_node # move the previous pointer forward
+            current_node = current_node.next # move the current pointer forward
+            
+        if current_node is None: # if the element was not found in the list
             return        
-        elif previous_node is not None:
-            previous_node.next = current_node.next
-        else:
-            self.head = current_node.next
-        self.length -= 1
+        elif previous_node is not None: # if the element is in the middle or end
+            previous_node.next = current_node.next # bypass the current node to remove it
+        else: # if the element to be removed is the first node (head)
+            self.head = current_node.next # set the second node as the new head
+            
+        self.length -= 1 # decrease the list size counter
 
+# Testing the code
 my_list = LinkedList()
-print(my_list.is_empty())
+print(my_list.is_empty()) # checks if list is empty (True)
 
-my_list.add(1)
-my_list.add(2)
-print(my_list.is_empty())
-print(my_list.length)
+my_list.add(1) # adds 1 to the list
+my_list.add(2) # adds 2 to the list
+print(my_list.is_empty()) # checks if list is empty (False)
+print(my_list.length) # prints the current length (2)
 
-my_list.remove(1)
-print(my_list.length)
+my_list.remove(1) # removes the element 1
+print(my_list.length) # prints the updated length (1)
