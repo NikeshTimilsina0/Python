5ca62aa8c873d005c0efb9bcee6d38ee719153be
Nikesh Timilsina <nikeshtimilsina3@gmail.com>
2026-02-09 10:18:10 +0545

    Added print to truncate the text

--- freecodecamp/daily_challenges/#178_Truncate_The_Text.py
+++ freecodecamp/daily_challenges/#178_Truncate_The_Text.py
@@ -4,9 +4,8 @@ def truncate_text(text):
 
     if len(text)>20:
         return text[:17]+'...'
-    print(text)
     return text
-truncate_text("Hello, world!")
-truncate_text("This string should get truncated.")
-truncate_text("Exactly twenty chars")
-truncate_text(".....................")
+print(truncate_text("Hello, world!"))
+print(truncate_text("This string should get truncated."))
+print(truncate_text("Exactly twenty chars"))
+print(truncate_text("....................."))
