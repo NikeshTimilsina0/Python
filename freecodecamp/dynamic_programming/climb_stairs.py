de69008f30f3d1bec8c506989a3666a5c54c2973
Nikesh Timilsina <nikeshtimilsina3@gmail.com>
2026-02-21 22:51:05 +0545

    Practiced dynamic programming climb stairs through memorization

--- freecodecamp/dynamic_programming/climb_stairs.py
+++ freecodecamp/dynamic_programming/climb_stairs.py
@@ -1,14 +1,11 @@
-def climb_stairs_memo(n, memo={}):
+def climb_stairs_memo(n,memo={}):
     """Dynamic programming with memoization"""
-    # Check if we've already calculated this value
+
     if n in memo:
-        return memo[n]  # Return cached result - O(1) lookup!
-    
-    # Base cases
-    if n <= 2:
+        return memo[n]
+    if n<=2:
         return n
-    
-    # Calculate once and store in memo for future use
     memo[n] = climb_stairs_memo(n-1, memo) + climb_stairs_memo(n-2, memo)
+    print(memo)
     return memo[n]
-print(climb_stairs_memo(50))  # Example usage
+print(climb_stairs_memo(5))
